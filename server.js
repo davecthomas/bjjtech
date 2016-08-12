ca5a09055ce97f021c2bb99161e69f33fd77efe2
davecthomas <dave@goverb.com>
2016-08-12 10:21:29 -0500

    cors

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ if ( 'development' == app.get( 'env' ) ) {
 var router = express.Router(); // get an instance of the express Router
 
 var root = require( './routes/root' ); // root.js has our web logic
-router.all( '/', function( req, res, next ) {
+app.all( '/', function( req, res, next ) {
   res.header( "Access-Control-Allow-Origin", "*" );
   res.header( "Access-Control-Allow-Headers", "X-Requested-With" );
   next();
