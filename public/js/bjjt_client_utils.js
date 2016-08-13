af7c5f2178681f98bead8a19822e9b3cf81f747e
davecthomas <dave@goverb.com>
2016-08-13 16:45:45 -0500

    replace numbers and move CTA up

--- public/js/bjjt_client_utils.js
+++ public/js/bjjt_client_utils.js
@@ -2,6 +2,19 @@ function replaceAll( str, search, replacement ) {
   return str.replace( new RegExp( search, 'g' ), replacement );
 };
 
+function replaceAllNumlist( str, replacement ) {
+  re = new RegExp( /\d+(\.)/, 'g' );
+  return str.replace( re, replacement );
+};
+
+// function breakBeforeNum(
+//   {[0-9]+.}
+// )
+function insertAt( src, index, str ) {
+  return src.substr( 0, index ) + str + src.substr( index )
+}
+
+
 function isNumeric( n ) {
   return !isNaN( parseFloat( n ) ) && isFinite( n );
 };
