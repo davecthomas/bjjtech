0ae7a3b8dc1d873bd366bb902ebd89340ec11d87
Dave Thomas <davidcthomas@fb.com>
2020-09-14 08:42:59 -0700

    fake React component added

--- public/js/react/bjjt_react.js
+++ public/js/react/bjjt_react.js
@@ -1,14 +1,15 @@
-      class Technique extends React.Component {
-        constructor() {
-          super();
-          // this.props = {color: "red"};
-        }
-        render() {
-          return <h4>{this.props.context} Technique!</h4>;
-        }
-      }
+class Technique extends React.Component {
+    constructor() {
+        super();
+        // this.props = {color: "red"};
+    }
+    render() {
+        return <a href="#" class="list-group-item get_techs_topic"><h4 class="list-group-item-heading">{this.props.topic}</h4></a>;
+    }
+}
+// export default Technique;
 
-      ReactDOM.render(
-          <Technique context="foo"/>,
-          document.getElementById('app')
-      );
+ReactDOM.render(
+    <Technique topic="foo"/>,
+    document.getElementById('app')
+);
